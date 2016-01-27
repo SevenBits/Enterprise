c38fc4ae4ac029a204abdbe42fe05828beffb27b
SevenBits <meokid2@yahoo.com>
2016-01-27 15:56:26 -0500

    Eliminate unnecessary whitespace

--- src/main.c
+++ src/main.c
@@ -83,7 +83,6 @@ EFI_STATUS efi_main(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE *systab) {
 	/* Check to make sure that we have our configuration file and GRUB bootloader. */
 	if (!FileExists(root_dir, L"\\efi\\boot\\enterprise.cfg")) {
 		// Check if we have an old-style configuration file instead.
-		
 		if (!FileExists(root_dir, L"\\efi\\boot\\.MLUL-Live-USB")) {
 			DisplayErrorText(L"Error: can't find configuration file.\n");
 			can_continue = FALSE;
