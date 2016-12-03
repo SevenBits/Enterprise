d4fb5c7869e073746ee7136b2aff288312cbb0ad
SevenBits <meokid2@yahoo.com>
2016-12-02 23:45:04 -0500

    Forgot to add GPLv3 notices to a few files

--- src/installer/main.c
+++ src/installer/main.c
@@ -2,15 +2,14 @@
  * Tool intended to help facilitate the process of booting Linux on Intel
  * Macintosh computers made by Apple from a USB stick or similar.
  *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation; either version 2.1 of the License, or
- * (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of version 3 of the GNU General Public License as
+ * published by the Free Software Foundation.
  *
  * This program is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
+ * General Public License for more details.
  *
  * Copyright (C) 2015 SevenBits
  *
@@ -166,8 +165,8 @@ static const char* check_search_path(void) {
 	return NULL;
 }
 
-static bool copy_file(const const char*, const const char*) __attribute__((nonnull (1,2)));
-static bool copy_file(const const char *source, const const char *destination) {
+static bool copy_file(const char * const, const char* const) __attribute__((nonnull (1,2)));
+static bool copy_file(const char * const source, const char * const destination) {
 	char *buffer;
 	FILE *inFilePointer = fopen(source, "rb");
 	FILE *outFilePointer = fopen(destination, "wb");
